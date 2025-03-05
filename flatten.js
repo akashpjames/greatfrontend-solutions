c70e1c43af843d585436e7752580a2921069994e
Akash Pulikkottil <akashpjames@gmail.com>
2025-03-05 16:35:42 -0500

    Update

--- flatten.js
+++ flatten.js
@@ -15,18 +15,48 @@ flatten([1, [2, [3, [4, [5]]]]]); // [1, 2, 3, 4, 5]
 */
 
 export default function flatten(value) {
-    const results = [];
-    const len = value.length;
-    for(let i=0; i<len; i++) {
-      if(Array.isArray(value[i])) {
-        results.push(...flatten(value[i]));
-        //If you use concat, make sure to add it back to the results variable
-        //concat doesn't mutate original array, but uses additional array.
-        //Performance: push(...flatten(...)) is usually more performant because it doesn’t involve creating a new array.
-        //results = results.concat(flatten(value[i]));
-      } else {
-        results.push(value[i]);
-      }
+
+    // Recursive
+    // if(Array.isArray(value)) {
+    //   const result = [];
+    //   for(const val of value) {
+    //     result.push(...flatten(val));
+    //   }
+    //   return result;
+    // } else {
+    //   return [value];
+    // }
+
+    // DFS - we have to push elements from right to left
+    const result = [];
+    const stack = [value];
+    while(stack.length) {
+        const current = stack.pop();
+        if(Array.isArray(current)) {
+            for (let i = current.length - 1; i >= 0; i--) {
+                stack.push(current[i]);
+            }
+        } else {
+            result.push(current);
+        }
     }
-    return results;
+    return result;
+
+    // THIS WON'T WORK
+    // Iterative BFS takes outer layers first, so we won't get the desired order
+    // flatten([[null, [true]], undefined])
+    // We will get [undefined, null, true] - BFS considers outer layers first.
+    // DFS with reverse pushing is the way to go here then!!!
+    // const result = [];
+    // const queue = [value];
+    // while (queue.length) {
+    //   const current = queue.shift(); // take the next element in the queue
+    //   if (Array.isArray(current)) {
+    //     // enqueue all elements in their natural order
+    //     queue.push(...current);
+    //   } else {
+    //     result.push(current);
+    //   }
+    // }
+    // return result;
   }
